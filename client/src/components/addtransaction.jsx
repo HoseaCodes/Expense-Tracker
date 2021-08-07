c465a7b63ee103bbb7c20bbfd6fabc6e2fd34e54
hosead6168 <dominique11h@yahoo.com>
2021-08-07 11:00:36 -0500

    refactor(category): added existing categories to select

--- client/src/components/addtransaction.jsx
+++ client/src/components/addtransaction.jsx
@@ -4,8 +4,9 @@ import { GlobalContext } from '../context/GlobalState'
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('');
 
-    const {addTransaction} = useContext(GlobalContext)
+    const {categories, addTransaction} = useContext(GlobalContext)
 
     const onSubmit = e => {
         e.preventDefault();
@@ -13,7 +14,8 @@ export const AddTransaction = () => {
         const newTransaction = {
             id: Math.floor(Math.random() * 1000000000),
             text,
-            amount: +amount
+            amount: +amount,
+            category
         }
 
         addTransaction(newTransaction);
@@ -23,15 +25,31 @@ export const AddTransaction = () => {
             <h3>Add new transaction</h3>
             <form onSubmit={onSubmit}>
                 <div className="form-control">
-                <label htmlFor="text">Text</label>
-                <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+                    <label htmlFor="text">Text</label>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
+                
                 <div className="form-control">
-                <label htmlFor="amount"
-                    >Amount <br />
-                    (negative - expense, positive - income)</label
-                >
-                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                    <label htmlFor="categories">Categories</label>
+                    <select name="category" value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    >
+                        {
+                            categories.map(category => (
+                                <option value={category._id} key={category._id}>
+                                    {category.name}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="amount"
+                        >Amount <br />
+                        (negative - expense, positive - income)</label
+                    >
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
