41efc128048130eb8e1998f0aaf81165ee8f4af2
hosead6168 <dominique11h@yahoo.com>
2021-09-17 21:59:20 -0500

    feat(router): added 3 new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { GlobalProvider } from './context/GlobalState';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/home';
-import About from './pages/about';
+import About from './pages/about/about';
 import Plaid from './pages/plaid';
-import FAQ from './pages/faq';
+import FAQ from './pages/faq/faq';
+import Settings from './pages/settings/settings';
+import Access from './pages/acess';
+import Profile from './pages/profile/profile';
 
 function App() {
     return (
@@ -13,6 +16,9 @@ function App() {
                 <Route exact path="/faq" render={() => <FAQ />} />
                 <Route exact path="/about" render={() => <About />} />
                 <Route exact path="/plaid" render={() => <Plaid />} />
+                <Route exact path="/profile" render={() => <Profile />} />
+                <Route exact path="/manageacess" render={() => <Access />} />
+                <Route exact path="/settings" render={() => <Settings />} />
             </Router>
         </GlobalProvider>
     );
